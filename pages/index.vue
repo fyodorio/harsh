646e87538658ec50e6aad04dce627f252763b399
fyodorio <alinetofyodor@gmail.com>
2021-01-19 12:34:05 +0500

    feat() style home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,23 @@
 <template>
   <div class="flex h-screen w-screen">
     <div class="m-auto text-center">
-      <h1 class="text-2xl font-bold">Harsh Code Review Rehab</h1>
-      <NuxtLink to="/review">Get Review</NuxtLink>
+      <p class="text-gray-700 md:text-xl uppercase">
+        Ministry Of Dev Mental Health Affairs Presents
+      </p>
+      <h2 class="md:text-6xl text-3xl font-bold mb-8 leading-tight">
+        <span class="text-red-400">Harsh</span>
+        <span class="text-blue-400">Code</span>
+        <span class="text-orange-500">Review</span>
+        <span class="text-green-500">Rehab</span>
+      </h2>
+      <NuxtLink
+        to="/review"
+        class="bg-green-500 hover:bg-green-700 text-white md:text-3xl text-xl font-bold py-4 px-8 rounded"
+        >Get Your Review</NuxtLink
+      >
+      <p class="mt-6 md:font-bold text-green-700">
+        <em>*</em>100% Approval Guarantee
+      </p>
     </div>
   </div>
 </template>
