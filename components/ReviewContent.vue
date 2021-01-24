801723bfbfbfdffd28bef500f5446c627e3cd545
fyodorio <alinetofyodor@gmail.com>
2021-01-24 15:45:08 +0500

    feat() main elements and controls for review
    add profile url

--- components/ReviewContent.vue
+++ components/ReviewContent.vue
@@ -6,11 +6,36 @@
         :src="content.avatar"
         :alt="content.name"
       />
-      <div class="mx-2">{{ content.name }}</div>
+      <a
+        :href="content.url"
+        class="mx-2 hover:underline font-bold hover:text-blue-700"
+        >{{ content.name }}</a
+      >
       <div class="text-gray-600">{{ content.createdAt }}</div>
+      <span class="flex-grow"></span>
+      <button>
+        <svg
+          class="fill-current text-gray-600 hover:text-blue-700"
+          viewBox="0 0 16 16"
+          version="1.1"
+          width="16"
+          height="16"
+          aria-hidden="true"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zM5 8a1 1 0 100-2 1 1 0 000 2zm7-1a1 1 0 11-2 0 1 1 0 012 0zM5.32 9.636a.75.75 0 011.038.175l.007.009c.103.118.22.222.35.31.264.178.683.37 1.285.37.602 0 1.02-.192 1.285-.371.13-.088.247-.192.35-.31l.007-.008a.75.75 0 111.222.87l-.614-.431c.614.43.614.431.613.431v.001l-.001.002-.002.003-.005.007-.014.019a1.984 1.984 0 01-.184.213c-.16.166-.338.316-.53.445-.63.418-1.37.638-2.127.629-.946 0-1.652-.308-2.126-.63a3.32 3.32 0 01-.715-.657l-.014-.02-.005-.006-.002-.003v-.002h-.001l.613-.432-.614.43a.75.75 0 01.183-1.044h.001z"
+          ></path>
+        </svg>
+      </button>
     </div>
-    <div class="my-2">{{ content.review }}</div>
-    <div class="text-gray-600">Controls</div>
+
+    <div class="my-3 text-xl">{{ content.review }}</div>
+
+    <div class="flex items-center text-gray-600">
+      <button class="mr-2 hover:text-blue-700">Reply</button>
+      <button class="hover:text-blue-700">Like</button>
+    </div>
   </div>
 </template>
 
@@ -25,6 +50,7 @@ export default {
           avatar:
             'https://pbs.twimg.com/profile_images/424700029916295168/ykqUxKm1.jpeg',
           name: 'Harry Potter',
+          url: 'https://en.wikipedia.org/wiki/Harry_Potter',
           review: 'Awesome',
           createdAt: '1d ago',
         }
--- static/data/data.js
+++ static/data/data.js
@@ -4,6 +4,7 @@ export const Reviews = [
     avatar:
       'https://pbs.twimg.com/profile_images/666991518121508864/sLu9f6s7_400x400.jpg',
     name: 'James Bond',
+    url: 'https://en.wikipedia.org/wiki/James_Bond',
     review: 'Awesome',
     createdAt: '2h ago',
   },
@@ -12,6 +13,7 @@ export const Reviews = [
     avatar:
       'https://pbs.twimg.com/profile_images/988775660163252226/XpgonN0X.jpg',
     name: 'Bill Gates',
+    url: 'https://en.wikipedia.org/wiki/Bill_Gates',
     review: `You're the man!`,
     createdAt: '1d ago',
   },
@@ -20,7 +22,17 @@ export const Reviews = [
     avatar:
       'https://pbs.twimg.com/profile_images/988775660163252226/XpgonN0X.jpg',
     name: 'Elon Musk',
+    url: 'https://en.wikipedia.org/wiki/Elon_Musk',
     review: `Me myself couldn't make it better`,
     createdAt: '2m ago',
+  },
+  {
+    id: '4',
+    avatar:
+      'https://avatars.githubusercontent.com/u/19417873?s=460&u=7963b945bb0c88caabb08b6e0283848757cc1d7d&v=4',
+    name: 'Fyodor',
+    url: 'https://fyodor.io',
+    review: `Keep up the great job! 💪`,
+    createdAt: 'just now',
   },
 ]
