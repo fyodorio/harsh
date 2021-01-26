4f6b2173bb2f15c02889b50282205669ef6bcc48
fyodorio <alinetofyodor@gmail.com>
2021-01-26 10:31:37 +0500

    feat() update review list

--- static/data/data.js
+++ static/data/data.js
@@ -1,38 +1,65 @@
 export const Reviews = [
   {
     id: '1',
-    avatar:
-      'https://pbs.twimg.com/profile_images/666991518121508864/sLu9f6s7_400x400.jpg',
-    name: 'James Bond',
-    url: 'https://en.wikipedia.org/wiki/James_Bond',
-    review: 'Awesome',
-    createdAt: '2h ago',
+    avatar: 'https://64.media.tumblr.com/avatar_43f7370603f5_128.pnj',
+    name: 'Richard Stallman',
+    url: 'https://en.wikipedia.org/wiki/Richard_Stallman',
+    review: 'I like this solution of yours, thorough and clean 👍',
+    createdAt: '1h ago',
   },
   {
     id: '2',
     avatar:
-      'https://pbs.twimg.com/profile_images/988775660163252226/XpgonN0X.jpg',
-    name: 'Bill Gates',
-    url: 'https://en.wikipedia.org/wiki/Bill_Gates',
-    review: `You're the man!`,
-    createdAt: '1d ago',
+      'https://res-5.cloudinary.com/crunchbase-production/image/upload/c_thumb,h_256,w_256,f_auto,g_faces,z_0.7,q_auto:eco/v1397181528/115e6fa16646bb4a44e532182b1dc57a.jpg',
+    name: 'Linus Torvalds',
+    url: 'https://en.wikipedia.org/wiki/Linus_Torvalds',
+    review: `I hate everyone's code, but I hate yours less now. Good job 🧐`,
+    createdAt: '2h ago',
   },
   {
     id: '3',
     avatar:
-      'https://d3g9pb5nvr3u7.cloudfront.net/authors/539a28913f3c0fd71ed4e43d/2131300937/256.jpg',
-    name: 'Elon Musk',
-    url: 'https://en.wikipedia.org/wiki/Elon_Musk',
-    review: `Me myself couldn't make it better`,
-    createdAt: '2m ago',
+      'https://res-5.cloudinary.com/crunchbase-production/image/upload/c_thumb,h_256,w_256,f_auto,g_faces,z_0.7,q_auto:eco/sknrf8z5f6u0wqntqh3h',
+    name: 'Mitchell Baker',
+    url: 'https://en.wikipedia.org/wiki/Mitchell_Baker',
+    review: 'Hilarious job! Welcome to the top! 🤗',
+    createdAt: '30m ago',
   },
   {
     id: '4',
     avatar:
+      'https://pbs.twimg.com/profile_images/988775660163252226/XpgonN0X.jpg',
+    name: 'Bill Gates',
+    url: 'https://en.wikipedia.org/wiki/Bill_Gates',
+    review: `You rule! I would hire you right now, but I'm out of business these days. Keep it up, Maestro 👨‍🎓`,
+    createdAt: '3h ago',
+  },
+  {
+    id: '5',
+    avatar:
+      'https://d3g9pb5nvr3u7.cloudfront.net/authors/539a28913f3c0fd71ed4e43d/2131300937/256.jpg',
+    name: 'Elon Musk',
+    url: 'https://en.wikipedia.org/wiki/Elon_Musk',
+    review: `Me myself couldn't make it better. Thanks, you've just made my day 🔥`,
+    createdAt: '2m ago',
+  },
+  {
+    id: '6',
+    avatar:
+      'https://res-1.cloudinary.com/crunchbase-production/image/upload/c_thumb,h_256,w_256,f_auto,g_faces,z_0.7,q_auto:eco/u58rwhtwx0wmvwztytbf',
+    name: 'Tim Berners-Lee',
+    url: 'https://en.wikipedia.org/wiki/Tim_Berners-Lee',
+    review:
+      'The Internet has become a little better with this PR of yours! Thank you! ❤️',
+    createdAt: '4h ago',
+  },
+  {
+    id: '7',
+    avatar:
       'https://avatars.githubusercontent.com/u/19417873?s=460&u=7963b945bb0c88caabb08b6e0283848757cc1d7d&v=4',
     name: 'Fyodor',
-    url: 'https://fyodor.io',
-    review: `Keep up the great job! 💪`,
+    url: 'https://github.com/fyodorio',
+    review: `Your code is awesome! Keep up the great job! 💪`,
     createdAt: 'just now',
   },
 ]
