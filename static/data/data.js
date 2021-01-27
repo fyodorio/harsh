937834a285788124ebd32c7b0a218fef788c19dc
fyodorio <alinetofyodor@gmail.com>
2021-01-27 10:03:07 +0500

    feat() add another review and basic-like ids

--- static/data/data.js
+++ static/data/data.js
@@ -1,6 +1,6 @@
 export const Reviews = [
   {
-    id: '1',
+    id: '10',
     avatar: 'https://64.media.tumblr.com/avatar_43f7370603f5_128.pnj',
     name: 'Richard Stallman',
     url: 'https://en.wikipedia.org/wiki/Richard_Stallman',
@@ -8,7 +8,16 @@ export const Reviews = [
     createdAt: '1h ago',
   },
   {
-    id: '2',
+    id: '20',
+    avatar:
+      'https://images.squarespace-cdn.com/content/v1/5d13c749eefd1100016a3eae/1565965789316-YKVWKVRY39OFHSLDOG91/ke17ZwdGBToddI8pDm48kO2pS9cMHbO4RvHPc8Skf1tZw-zPPgdn4jUwVcJE1ZvWhcwhEtWJXoshNdA9f1qD7SSwGn0TPzISNt3iSJufpcvR7xFZ2oYA-YTitnkXPCuTgiUfhLEJ_Uxi_cK3qclb8w/Mark_Dean.png',
+    name: 'Mark Dean',
+    url: 'https://en.wikipedia.org/wiki/Mark_Dean_(computer_scientist)',
+    review: `Clever thing... Very clever... Well done! Admit, you have trained to do that, haven't you?`,
+    createdAt: '1h ago',
+  },
+  {
+    id: '30',
     avatar:
       'https://res-5.cloudinary.com/crunchbase-production/image/upload/c_thumb,h_256,w_256,f_auto,g_faces,z_0.7,q_auto:eco/v1397181528/115e6fa16646bb4a44e532182b1dc57a.jpg',
     name: 'Linus Torvalds',
@@ -17,7 +26,7 @@ export const Reviews = [
     createdAt: '2h ago',
   },
   {
-    id: '3',
+    id: '40',
     avatar:
       'https://res-5.cloudinary.com/crunchbase-production/image/upload/c_thumb,h_256,w_256,f_auto,g_faces,z_0.7,q_auto:eco/sknrf8z5f6u0wqntqh3h',
     name: 'Mitchell Baker',
@@ -26,7 +35,7 @@ export const Reviews = [
     createdAt: '30m ago',
   },
   {
-    id: '4',
+    id: '50',
     avatar:
       'https://pbs.twimg.com/profile_images/988775660163252226/XpgonN0X.jpg',
     name: 'Bill Gates',
@@ -35,7 +44,7 @@ export const Reviews = [
     createdAt: '3h ago',
   },
   {
-    id: '5',
+    id: '60',
     avatar:
       'https://d3g9pb5nvr3u7.cloudfront.net/authors/539a28913f3c0fd71ed4e43d/2131300937/256.jpg',
     name: 'Elon Musk',
@@ -44,7 +53,7 @@ export const Reviews = [
     createdAt: '2m ago',
   },
   {
-    id: '6',
+    id: '70',
     avatar:
       'https://res-1.cloudinary.com/crunchbase-production/image/upload/c_thumb,h_256,w_256,f_auto,g_faces,z_0.7,q_auto:eco/u58rwhtwx0wmvwztytbf',
     name: 'Tim Berners-Lee',
@@ -54,7 +63,7 @@ export const Reviews = [
     createdAt: '4h ago',
   },
   {
-    id: '7',
+    id: '80',
     avatar:
       'https://avatars.githubusercontent.com/u/19417873?s=460&u=7963b945bb0c88caabb08b6e0283848757cc1d7d&v=4',
     name: 'Fyodor',
